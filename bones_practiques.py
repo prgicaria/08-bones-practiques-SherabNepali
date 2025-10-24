3e551b1a714bb2c0ce5ed39d670b0bf5b9522303
Sherab_Nepali <snepali@instituticaria.cat>
2025-10-24 10:49:13 +0200

    b)

--- bones_practiques.py
+++ bones_practiques.py
@@ -5,13 +5,13 @@
 Institut Icària
 Programació - 1r Batxillerat - Curs 2025-26
 
-El programa calcula el quocient i el residu d'una divisió en la qual tú dones 
-el valor del dividend i el valor del divisor. En donar els dos valors el 
-programa executa la divisió i inidica a la pantalla la divisió en forma de 
+El programa calcula el quocient i el residu d'una divisió en la qual tú dones
+el valor del dividend i el valor del divisor. En donar els dos valors el
+programa executa la divisió i inidica a la pantalla la divisió en forma de
 fracció, el quocient i el residu de la divisió'''
 
-__author__ = "Sherab Odsel Nepali Dolkar" 
-__email__ = "snepali@instituticaria.cat" 
+__author__ = "Sherab Odsel Nepali Dolkar"
+__email__ = "snepali@instituticaria.cat"
 __date__ = "22/10/2025"
 
 dividend = int(input("Introdueix el dividend:"))
